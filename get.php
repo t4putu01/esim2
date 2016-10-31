d4be18ae5e1a73843e9b81df15ce60859a097c26
Tuomas <t4putu01@students.oamk.fi>
2016-10-31 10:33:19 +0200

    Get lomake luotu

---
+++ get.php
@@ -0,0 +1,19 @@
+<h1>Testataan get-metodia</h1>
+
+<form method="get" action="get.php">
+<label>Etunimi</label>
+<input type="text" name ="en" required="">
+<br>
+<label>Sukunimi</label>
+<input type="text" name="sn" required="">
+<br>
+<input type="submit" name="nappi" value="Lähetä">
+</form>
+
+<?php
+if(isset($_GET['nappi'])) {
+$etunimi=$_GET['en'];
+$sukunimi=$_GET['sn'];
+echo "Terve $etunimi $sukunimi";
+}
+?>
